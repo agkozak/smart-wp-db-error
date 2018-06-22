2dbb2137aba71e96474d41549a0d3fd87eb06acd
Alexandros Kozak <git-agkozak@sneakemail.com>
2018-06-22 09:31:40 -0700

    Code style and linting configuration

---
+++ .phan/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This configuration will be read and overlaid on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ */
+return [
+	'include_analysis_file_list' => ['db-error.php.dist']
+];
