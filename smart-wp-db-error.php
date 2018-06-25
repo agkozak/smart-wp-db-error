6f560abce567406dd1cc76c9e2d6c56c1479daf2
Alexandros Kozak <git-agkozak@sneakemail.com>
2018-06-24 17:38:05 -0700

    Code cleanup

--- smart-wp-db-error.php
+++ smart-wp-db-error.php
@@ -18,7 +18,7 @@ if ( ! defined( 'MAIL_FROM' )
 
 // Information protocol of incoming request.
 if ( isset( $_SERVER['SERVER_PROTOCOL'] ) ) {
-    $server_protocol = $_SERVER['SERVER_PROTOCOL'];
+	$server_protocol = $_SERVER['SERVER_PROTOCOL'];
 } else {
 	$server_protocol = 'HTTP/1.1';
 }
@@ -31,7 +31,7 @@ $lock    = __DIR__ . DIRECTORY_SEPARATOR . 'smart-wp-db-error.lock';
 // When db-error.php is accessed directly, only show the message; do not e-mail.
 if ( defined( 'ABSPATH' ) ) {
 
-    // If lock exists and is older than the alert interval, delete it.
+	// If lock exists and is older than the alert interval, delete it.
 	if ( file_exists( $lock ) ) {
 		if ( time() - filectime( $lock ) > ALERT_INTERVAL ) {
 			unlink( $lock );
@@ -39,37 +39,37 @@ if ( defined( 'ABSPATH' ) ) {
 
 	// Otherwise try to create the lock; if successful, send the alert e-mail.
 	} elseif ( touch( $lock ) ) {
-		$touched  = true;
-		$headers  = 'From: ' . MAIL_FROM . "\n" .
+		$touched = true;
+		$headers = 'From: ' . MAIL_FROM . "\n" .
 			'X-Mailer: PHP/' . PHP_VERSION . "\n" .
 			'X-Priority: 1 (High)';
 
 		// Encrypted vs. non-encrypted connection.
-		if ( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' ) {
-		    $web_protocol = 'https';
-        } else {
-		    $web_protocol = 'http';
-        }
-
-        // Server name.
-        if ( isset( $_SERVER['SERVER_NAME'] ) ) {
-		    $server_name = filter_var( stripslashes(
-		            $_SERVER['SERVER_NAME']                   // Input var okay.
-            ), FILTER_SANITIZE_URL );
-        } else {
-		    $server_name = '';
-        }
-
-        // Request URI.
-        if ( isset( $_SERVER['REQUEST_URI'] ) ) {
-		    $request_uri = filter_var( stripslashes(
-		            $_SERVER['REQUEST_URI']                   // Input var okay.
-				), FILTER_SANITIZE_URL );
+		if ( ! empty( $_SERVER['HTTPS'] ) && 'off' !== $_SERVER['HTTPS'] ) {
+			$web_protocol = 'https';
 		} else {
-		    $request_uri = '';
-        }
+			$web_protocol = 'http';
+		}
 
-        // The e-mail alert.
+		// Server name.
+		if ( isset( $_SERVER['SERVER_NAME'] ) ) {
+			$server_name = filter_var( stripslashes(
+				$_SERVER['SERVER_NAME']                       // Input var okay.
+			), FILTER_SANITIZE_URL );
+		} else {
+			$server_name = '';
+		}
+
+		// Request URI.
+		if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+			$request_uri = filter_var( stripslashes(
+				$_SERVER['REQUEST_URI']                   // Input var okay.
+			), FILTER_SANITIZE_URL );
+		} else {
+			$request_uri = '';
+		}
+
+		// The e-mail alert.
 		$message = 'Database Error on ' . $server_name . "\n" .
 			'The database error occurred when someone tried to open this page: '
 			. $web_protocol . '://' . $server_name . $request_uri . "\n";
@@ -133,9 +133,9 @@ if ( defined( 'ABSPATH' ) ) {
 				<h1>Database Error</h1>
 				<p>Sorry for the inconvenience.  Check back later.</p>
 				<?php
-                if ( true === $touched
-                     || ( file_exists( $lock )
-                          && time() - filectime( $lock ) <= ALERT_INTERVAL ) ) :
+				if ( true === $touched
+					|| ( file_exists( $lock )
+					&& time() - filectime( $lock ) <= ALERT_INTERVAL ) ) :
 				?>
 				<p>Administrator alerted.</p>
 				<?php endif; ?>
